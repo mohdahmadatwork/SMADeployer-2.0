4d2682a50621d1e99a3bf232f091a1bee9be2c1f
Mohd Ahmad <mohdahmadatwork@gmail.com>
2024-03-17 23:35:43 +0530

    Vercel 3.O

--- api-server/index.js
+++ api-server/index.js
@@ -2,11 +2,37 @@ const express = require('express');
 const app = express();
 const port = 9000;
 const {ECSClient,RunTaskCommand} = require('@aws-sdk/client-ecs');
-const Redis = require('ioredis');
+const {createClient} = require('@clickhouse/client');
 const {Server, Socket} = require('socket.io');
+const { Kafka} = require('kafkajs');
+const { v4: uuidv4} = require('uuid');
+const {fs} = require('fs');
+const {path} = require('path');
 app.use(express.json());
-const subscriber = new Redis(process.env.REDIS_SERVER);
+const client = new createClient({
+    host:process.env.CLICK_HOUSE_URL,
+    database:process.env.CLICK_HOUSE_DB,
+    username:process.env.CLICK_HOUSE_USERNAME,
+    password:process.env.CLICK_HOUSE_PASSWORD,
+})
 const io = new Server({cors:'*'});
+const { z } = require('zod');
+const { PrismaClient } = require('@prisma/client');
+const { table } = require('console');
+const prisma = new PrismaClient({});
+const kafka = new Kafka({
+    clientId:`api-server`,
+    brokers:[process.env.KAFKA_BROKER_URL],
+    ssl:{
+        ca:[fs.readFileSync(path.join(__dirname,'kafka.pem'),'utf-8')]
+    },
+    sasl:{
+        username:process.env.KAFKA_BROKER_USERNAME,
+        password:process.env.KAFKA_BROKER_PASSWORD,
+        mechanism:process.env.KAFKA_BROKER_MECHANISM
+    }
+});
+const consumer = kafka.consumer({groupId:`api-server-logs-consumer`});
 const ecsClient = new ECSClient({
     region: process.env.AWS_REGION,
     credentials:{
@@ -18,8 +44,34 @@ const config = {
     CLUSTER: 'cluster arn',
     TASK:'task arn'
 }
-app.post('/project',async (req,res)=>{
-    const {gitUrl} = req.body;
+app.post('/project',async(req,res)=>{
+    const schema = z.object({
+        name:z.string(),
+        gitUrl:z.string()
+    });
+    const safeParseResult = schema.safeParse(req.body);
+    if (safeParseResult.error) {
+        return res.status(400).json({error:safeParseResult.error});
+    }
+    const project = await prisma.project.create({
+        data:{
+            name,
+            gitUrl,
+            subDomain:generate()
+        }
+    });
+    return res.json({status:"success",data:{project}});
+})
+app.post('/deploy',async (req,res)=>{
+    const {projectId} = req.body;
+    const project = prisma.project.findUnique({where:{id:projectId}});
+    if(!project) return res.status(404).json({error:"Project nopt found"});
+    const deployment = await prisma.deployment.create({
+        data:{
+            project:{connect:{id:projectId}},
+            status:'QUEUED'
+        }
+    });
     const projectSlug = generate();
     const command = new RunTaskCommand({
         cluster:config.CLUSTER,
@@ -37,8 +89,8 @@ app.post('/project',async (req,res)=>{
                 {
                     name:'builder-image',//image name
                     environment:[
-                        {name:'PROJECT_ID',value:projectSlug},
-                        {name:'GIT_REPOSITORY_URL',value:process.env.GIT_REPOSITORY_URL},
+                        {name:'PROJECT_ID',value:projectId},
+                        {name:'GIT_REPOSITORY_URL',value:project.gitUrl},
                         {name:'AWS_REGION',value:process.env.AWS_REGION},
                         {name:'AWS_ACCESS_KEY_ID',value:process.env.AWS_ACCESS_KEY_ID},
                         {name:'AWS_SECRET_ACCESS_KEY',value:process.env.AWS_SECRET_ACCESS_KEY},
@@ -49,7 +101,7 @@ app.post('/project',async (req,res)=>{
     })
 
     const response = await ecsClient.send(command);
-    return res.status({status:'queued',data:{projectSlug,url:`http://${projectSlug}.localhost:${port}`}});
+    return res.status({status:'queued',data:{projectId,url:`http://${projectSlug}.localhost:${port}`}});
 });
 io.on('connection',socket=>{
     socket.on('subscribe',channel=>{
@@ -57,13 +109,30 @@ io.on('connection',socket=>{
         socket.emit("message",`Joined ${channel}`)
     })
 })
-async function initRedisSubscribe(){
-    console.log("Subscribed to logs...");
-    subscriber.psubscribe('logs:*');
-    subscriber.on('pmessage',(pattern,channel,message)=>{
-        io.to(channel).emit('message',message);
-    });
+
+async function initiateKafkaConsumer(){
+    await consumer.connect();
+    await consumer.subscribe({topic:['container-logs']});
+    await consumer.run({
+        autoCommit:false,
+        eachBatch: async function({batch,heartbeat,commitOffsetsIfNecessary,resolveOffset}){
+            const messages = batch.messages;
+            console.log("Rec ",messages.length," messages");
+            for (const message of messages){
+                const stringMessage= message.value.toString();
+                const {PROCESS_ID,DEPLOYMENT_ID,logs} = JSON.parse(stringMessage);
+                const {query_id} = await client.insert({
+                    table:'log_events',
+                    values: [{event_id: uuidv4(), deployment_id: DEPLOYMENT_ID,logs}]
+                });
+                resolveOffset(message.offset)
+                await commitOffsetsIfNecessary(message.offset)
+                await heartbeat()
+            }
+        }
+    })
 }
+initiateKafkaConsumer();
 function generate(){
     let ans = "";
     const subset = "0123456789qwertyuiopasdfghjklzxcvbnm";
@@ -72,7 +141,7 @@ function generate(){
     }
     return ans;
 }
-initRedisSubscribe();
+
 app.listen(port,()=>{
     console.log("API server serving on port: ",port);
 })
--- build_server/script.js
+++ build_server/script.js
@@ -4,7 +4,7 @@ const path = require("path");
 const {S3Client,PutObjectCommand} = require("@aws-sdk/client-s3");
 const AWS = require('aws-sdk');
 const mime = require("mime-types");
-const Redis = require('ioredis');
+const { Kafka} = require('kafkajs');
 const s3Client = new S3Client({
     region: process.env.AWS_REGION ,
     Credential:{
@@ -18,31 +18,46 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
-const PROJECT_ID = process.env.PROJECT_ID
+const PROJECT_ID = process.env.PROJECT_ID;
+const DEPLOYMENT_ID = process.env.DEPLOYMENT_ID;
+const kafka = new Kafka({
+    clientId:`docker-build-server-${DEPLOYMENT_ID}`,
+    brokers:[process.env.KAFKA_BROKER_URL],
+    ssl:{
+        ca:[fs.readFileSync(path.join(__dirname,'kafka.pem'),'utf-8')]
+    },
+    sasl:{
+        username:process.env.KAFKA_BROKER_USERNAME,
+        password:process.env.KAFKA_BROKER_PASSWORD,
+        mechanism:process.env.KAFKA_BROKER_MECHANISM
+    }
+});
+const producer = kafka.producer(); 
 async function init(){
+    await producer.connect();
     console.log("Executing script.js");
-    publisLog('Build Started...');
+    await publisLog('Build Started...');
     const outDirPath = path.join(__dirname,'output');
     console.log("outDirPath",outDirPath);
     const p = exec(`cd ${outDirPath} && npm install && npm run build`);
     p.stdout.on('data',function(data){
         console.log(data.toString());
     });
-    p.stdout.on('Error',function(data){
+    p.stdout.on('Error',async function(data){
         console.log("Error:",data.toString());
-        publisLog(`Error: ${data.toString()}`);
+        await publisLog(`Error: ${data.toString()}`);
     });
     p.on('close',async function(){
         console.log("build completed");
         const distFolderPath = path.join(__dirname,'output','dist');
         console.log("distFolderPath",distFolderPath);
         const distFolderContent = fs.readdirSync(distFolderPath,{recursive:true});
-        publisLog('Starting to upload...');
+        await publisLog('Starting to upload...');
         for (const file of distFolderContent) {
             const filePath = path.join(distFolderPath, file);
             console.log("filePath",filePath);
             if (fs.lstatSync(filePath).isDirectory()) continue;
-            publisLog(`uploading ${file}`);
+            await publisLog(`uploading ${file}`);
             const command = new PutObjectCommand({
               Bucket: 'smavercel',
               Key: `__outputs/${PROJECT_ID}/${file}`,
@@ -50,14 +65,15 @@ async function init(){
               ContentType: mime.lookup(filePath)
             })
             await s3Client.send(command)
-            publisLog(`uploaded ${file}`);
+            await publisLog(`uploaded ${file}`);
             console.log('uploading ends', filePath)
         }
-        publisLog("Done...");
+        await publisLog("Done...");
+        process.exit(0);
     });
 }
-function publisLog(log){
-  publisher.publish(`logs:${PROJECT_ID}`,JSON.stringify(log));
+async function publisLog(log){
+  await producer.send({topic:`container-logs`,messages:[{key:'log',value:JSON.stringify({PROJECT_ID,DEPLOYMENT_ID,log})}]})
 }
 init(); 
   
